2ff8dfe2d022c6b683317b6264e5272386ec5013
Eddie Flores <ef2k@users.noreply.github.com>
2018-07-10 15:55:34 -0400

    Add track page

---
+++ src/renderer/components/TrackPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="track-page">
+    <h1></h1>
+    <input type="text"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'track-page',
+    props: [
+      'token',
+      'port'
+    ],
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/_globals";
+
+.track-page {
+}
+</style>
